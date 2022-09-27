00ff56f59f3e4dda8d838f577cafd42ea7ecb2f8
hiep-ph288 <hiep.ph288@gmail.com>
2022-09-27 16:33:41 +0700

    uploadimage

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -4,15 +4,97 @@
  * Upload: https://www.youtube.com/watch?v=YOAeBSCkArA
  */
 
-import { View, Text } from "react-native";
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { firebase } from "../firebase/config";
 
-const UploadImage = () => {
+export default function App() {
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const uploadImage = async () => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function () {
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", image, true);
+      xhr.send(null);
+    });
+    const ref = firebase.storage().ref().child(`Pictures/Image1`);
+    const snapshot = ref.put(blob);
+    snapshot.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      () => {
+        setUploading(true);
+      },
+      (error) => {
+        setUploading(false);
+        console.log(error);
+        blob.close();
+        return;
+      },
+      () => {
+        snapshot.snapshot.ref.getDownloadURL().then((url) => {
+          setUploading(false);
+          console.log("Download URL: ", url);
+          setImage(url);
+          blob.close();
+          return url;
+        });
+      }
+    );
+  };
+
   return (
-    <View>
-      <Text>UploadImage</Text>
+    <View style={styles.container}>
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 170, height: 200 }} />
+      )}
+      <Button title="Select Image" onPress={pickImage} />
+      {!uploading ? (
+        <Button title="Upload Image" onPress={uploadImage} />
+      ) : (
+        <ActivityIndicator size={"small"} color="black" />
+      )}
     </View>
   );
-};
+}
 
-export default UploadImage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1jWZI3yH5cxbeMMs5nsY44fA6kAnl6Kg",
@@ -13,7 +15,14 @@ const firebaseConfig = {
   measurementId: "G-84N288JTYC",
 };
 
+if (!firebase.app.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+const app = initializeApp(firebaseConfig)
+
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firestore = getFirestore(firebaseApp);
 export const firebaseAuth = getAuth(firebaseApp);
+export {app, firebase};
