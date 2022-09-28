b0c2ce52b13da6118dd24981ae1fd750cbfbafb1
greny98 <20164367@student.hust.edu.vn>
2022-09-28 19:13:55 +0700

    Interest

--- src/screens/Auth/InterestsVerification.tsx
+++ src/screens/Auth/InterestsVerification.tsx
@@ -3,6 +3,30 @@ import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
+const interests = [
+  "Gym",
+  "Đọc sách",
+  "Yoga",
+  "Spa",
+  "Thiền",
+  "Soundcloud",
+  "Cà phê",
+  "Du lịch",
+  "Guitar",
+  "Instagram",
+  "Xem phim",
+  "Hip hop",
+  "Giày Sneaker",
+  "Sushi",
+  "Chạy",
+  "Hockey",
+  "Chạy",
+  "Harry Potter",
+  "Suối nước nóng",
+  "Đi Concert",
+  "Massage"
+];
+
 const InterestsVerification = () => {
   const navigation = useNavigation<any>();
   const interestsVerification = () => {
@@ -12,24 +36,17 @@ const InterestsVerification = () => {
     <View>
       <View style={{ width: "100%" }}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <AntDesign
-            name="left"
-            size={30}
-            color="black"
-            style={{ marginVertical: 45, marginHorizontal: 15 }}
-          />
+          <AntDesign name="left" size={30} color="black" style={{ marginVertical: 45, marginHorizontal: 15 }} />
         </TouchableOpacity>
       </View>
 
       <View style={{ width: "80%", marginHorizontal: "10%" }}>
-        <Text style={{ fontSize: 26, fontWeight: "700" }}>
-          Sở Thích
-        </Text>
+        <Text style={{ fontSize: 26, fontWeight: "700" }}>Sở Thích</Text>
         <Text>Hãy cho mọi người biết bạn thích những gì bằng cách thêm sở thích vào hồ sơ</Text>
-        <View style={{flexDirection: 'row'}}>
-            <TouchableOpacity style={{borderWidth: 1, borderRadius: 60, paddingHorizontal: 15, paddingVertical: 5}}>
-                <Text>90S Kid</Text>
-            </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity style={{ borderWidth: 1, borderRadius: 60, paddingHorizontal: 15, paddingVertical: 5 }}>
+            <Text>90S Kid</Text>
+          </TouchableOpacity>
         </View>
       </View>
 
@@ -39,14 +56,12 @@ const InterestsVerification = () => {
           style={{
             width: 342,
             height: 56,
-            borderRadius: 60,
-            paddingHorizontal: 15,
-            marginTop: 50,
-            backgroundColor: "#F5344B",
+            borderRadius: 30,
+            marginTop: 20,
             alignItems: "center",
             justifyContent: "center",
             borderWidth: 1,
-            borderColor: "white",
+            // borderColor: gender.i !== -1 ? "black" : "gray",
           }}
         >
           <Text style={{ color: "white", fontWeight: "700" }}>TIẾP TỤC</Text>
