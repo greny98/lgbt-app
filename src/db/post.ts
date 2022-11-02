96f97aad1d0d02b064a257c50cf730d7635d4d18
hiep-ph288 <hiep.ph288@gmail.com>
2022-11-02 12:01:17 +0700

    post

--- src/db/post.ts
+++ src/db/post.ts
@@ -13,15 +13,18 @@ export interface PostProps {
   heart: string;
 }
 
-export const postData: PostProps[] = {
-  img:{require("../assets/images/avt.jpeg")},
-  name:"Martha Craig",
-  user:"@craig_love",
-  time:"12",
-  content:"UXR/UX: You can only bring one item to a remote island to assist your research of native use of tools and usability. What do you bring?",
-  hashtag:"#TellMeAboutYou",
-  img_content:{require("./assets/images/content.png")},
-  comment:"28",
-  retweet:"5",
-  heart:"21",
-};
+export const postData: PostProps[] = [
+  {
+    img: require("../assets/images/avt.jpeg"),
+    name: "Martha Craig",
+    user: "@craig_love",
+    time: "12",
+    content:
+      "UXR/UX: You can only bring one item to a remote island to assist your research of native use of tools and usability. What do you bring?",
+    hashtag: "#TellMeAboutYou",
+    img_content: require("./assets/images/content.png"),
+    comment: "28",
+    retweet: "5",
+    heart: "21",
+  },
+];
